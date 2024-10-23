871ded5a712c270bbe7a68e4d95f2e0bdf5b3b45
Daniel Santamarta <danisantamarta5@gmail.com>
2024-10-23 09:31:54 +0200

    creamos la clase abstracta "Miembro" que usaremos como clase padre a futuro

---
+++ clases/miembro.php
@@ -0,0 +1,126 @@
+<?php
+namespace clases;
+abstract class Miembro{
+    protected int $id;
+    protected $nombre;
+    protected $apellidos;
+    protected $email;
+    protected int $edad;
+
+    public function __construct($id,$nombre,$apellidos,$email,$edad){
+        $this->id=$id;
+        $this->nombre=$nombre;
+        $this->apellidos=$apellidos;
+        $this->email=$email;
+        $this->edad=$edad;
+    }
+
+
+
+    /**
+     * Get the value of edad
+     */ 
+    public function getEdad()
+    {
+        return $this->edad;
+    }
+
+    /**
+     * Set the value of edad
+     *
+     * @return  self
+     */ 
+    public function setEdad($edad)
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of apellidos
+     */ 
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * Set the value of apellidos
+     *
+     * @return  self
+     */ 
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nombre
+     */ 
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return "Nombre: ".$this->nombre." ".$this->apellidos.", Email: ".$this->email;
+    }
+    
+}
+?>
