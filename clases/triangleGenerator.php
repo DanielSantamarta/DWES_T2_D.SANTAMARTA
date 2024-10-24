0ed386480a83f222c99fa3d55f97090e18bea6be
Daniel Santamarta <danisantamarta5@gmail.com>
2024-10-24 14:12:20 +0200

    cambiamos el metodo generateTriangle para ajustarse mejor al texto del ejercicio y añadimos un test de prueba

--- clases/triangleGenerator.php
+++ clases/triangleGenerator.php
@@ -1,11 +1,17 @@
 <?php
-    function generateTriangle(int $altura) {
+    function generateTriangle(int $altura): string {
+        if ($altura < 0) {
+            return '';
+        }
         $triangulo = '';
         for ($i = 0; $i < $altura; $i++) {
-            $espacios = str_repeat(' ', $altura - $i - 1);
+            $espacios = str_repeat('&nbsp;', $altura - $i - 1);
             $asteriscos = str_repeat('*', 2 * $i + 1);
-            $triangulo += $espacios + $asteriscos + "\n";
+            $triangulo .= '<p>' . $espacios . $asteriscos . '</p>';
         }
-    
+        
         return $triangulo;
     }
+        
+    $altura = 7;
+    echo "<pre>".generateTriangle($altura)."</pre>";
